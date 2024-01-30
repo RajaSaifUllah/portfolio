b4502963b702e28ea5a509e0bb3ce6cc3662874b
Saif Ullah <su4090684@gmail.com>
2024-01-30 08:45:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import pic1 from './saif.jpeg';
+import pic2 from './saif2.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Saifi 😊</h1>
+        <h4>Hey! 'My name is Saif-Ullah'</h4>
+        <div className="logos">
+          {/* Include your logos here */}
+          <img src={pic1} alt="Logo 1" className="logo" />
+          <img src={pic2} alt="Logo 2" className="logo" />
+        </div>
       </header>
+      <section className="content">
+        <h2>Education and Experience</h2>
+        <p>
+          Undergraduate at Numl University Islamabad
+        </p>
+        <p>
+          Experience in C++, Java, CSS, HTML, JavaScript, Python, and ReactJs.
+        </p>
+        <div>
+          <p>Check out my React projects:</p>
+          <ul>
+            <li>
+              <a href="https://sports-shop-drab.vercel.app/" target="_blank" rel="noopener noreferrer">
+                sports-shop 
+              </a>
+            </li>
+            <li>
+              <a href="https://tyara.vercel.app/" target="_blank" rel="noopener noreferrer">
+                online-Bus-Service 
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <footer>
+        <h2>Contact Information</h2>
+        <p>Phone: 03175343491</p>
+        <p>Email: su2285926@gmail.com</p>
+        <p>Address: District Rawalpindi, Tehsil Gujar Khan, Post Office Khinger Mumdal, Village Mohri Rajgan</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
